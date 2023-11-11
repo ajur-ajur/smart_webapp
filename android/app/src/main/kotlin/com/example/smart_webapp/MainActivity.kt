da336b0530d0584a3996eb9b0382a315cb668a07
ajur-ajur <lazuardialdira.form@gmail.com>
2023-11-11 19:45:35 +0700

    fisrt

---
+++ android/app/src/main/kotlin/com/example/smart_webapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.smart_webapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:smart_webapp/src/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
