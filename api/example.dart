dd2dfa66b4598baa5f9b46d5cc0a3b11b0569515
ajurajur <lazuardialdira.form@gmail.com>
2023-12-22 17:06:47 +0700

    updated API index.js

--- api/example.dart
+++ api/example.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:smart_webapp/main.dart';
--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch').default;
+const date = require('date-and-time');
 
 const app = express();
 const port = 5000;
@@ -30,6 +32,43 @@ app.post('/rename', (req, res)=>{
     res.json({
         identity
     })
+})
+
+app.post('/transaction', async (req, res)=>{
+    url = 'https://app.sandbox.midtrans.com/snap/v1/transactions';
+    server_key = 'SB-Mid-server-KAV8I7fp2LZlDOEV9gM1Sy7q';
+
+    const now = new Date();
+    date.format(now, 'YYMMDDHHmm');
+
+    AUTH_STRING = Buffer.from(server_key+':').toString('base64');
+    
+    const headers = {
+        'Accept':'application/json',
+        'Content-Type':'application/json',
+        'Authorization': 'Basic ' + AUTH_STRING,
+    };
+
+    const body = {
+        'transaction_details': {
+            'order_id': now,
+            'gross_amount': 200000,
+        }
+    };
+
+    try{
+        const midtransResponse = await fetch(url,{
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(body),
+        });
+
+        const midtransData = await midtransResponse.json();
+        res.json(midtransData);
+    }catch(error){
+        console.error('Error calling API:', error);
+        res.status(500).json({error: 'Status error'});
+    }
 })
 
 app.listen(port, () => {
